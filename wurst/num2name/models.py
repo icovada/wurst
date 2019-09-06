fc93e9895e5cd92b4a4397445207aa259bea094b
Federico Tabbò <federico@tabbo.it>
2019-09-06 22:09:52 +0200

    Version 1.1
    Speeddial field is an integer
    Can be inserted as NULL from Django admin
    Directory representation adds speeddial

--- wurst/num2name/models.py
+++ wurst/num2name/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Directory(models.Model):
     name = models.TextField()
     number = models.TextField(unique=True)
-    speeddial = models.TextField(unique=True, null=True)
+    speeddial = models.IntegerField(unique=True, null=True, blank=True)
 
     def __str__(self):
-        return('{} - {}'.format(self.name, self.number))
+        return('{} - {} - {}'.format(self.name, self.number, self.speeddial))
