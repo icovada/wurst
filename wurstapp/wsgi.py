5b6bdc9c6fc17db41f07a94ddfbaec0dd3ebfed1
Icovada <ftabbo@gmail.com>
2020-03-07 12:40:51 +0100

    Docker it up

--- wurstapp/wsgi.py
+++ wurstapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wurst project.
+WSGI config for stageitweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
--- wurstapp/wurst/settings.py
+++ wurstapp/wurst/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -77,12 +76,24 @@ WSGI_APPLICATION = 'wurst.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
